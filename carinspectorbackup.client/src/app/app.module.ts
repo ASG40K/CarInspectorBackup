d5536a65f6cd86ae5826fc1015f12f278f800f4a
ASG40K <anthonygalindo185@gmail.com>
2025-04-10 16:20:27 -0700

    Add project files.

---
+++ carinspectorbackup.client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { GuestGarageComponent } from './guest-garage/guest-garage.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarsComponent,
+    GuestGarageComponent,
+    HomeComponent,
+    NavMenuComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
